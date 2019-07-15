cba72dbcc4ed64b1240117aef124dd45a5097296
lagadec <arthurlagadec64@gmail.com>
2019-07-15 15:34:25 +0200

    init

---
+++ alter_wizard.sql
@@ -0,0 +1,4 @@
+ALTER TABLE
+  wizard
+ADD
+  is_muggle BOOL NOT NULL;
---
+++ create_school.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `school` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  `capacity` INTEGER NULL,
+  `country` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
